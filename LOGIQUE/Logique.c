28ea4aa72e00688d9a6601bc318d4cf92f41a348
PaulCantier <cantierpaulo@gmail.com>
2021-05-17 17:36:52 +0200

    modif3

--- LOGIQUE/Logique.c
+++ LOGIQUE/Logique.c
@@ -7,19 +7,17 @@
 bool play(int currentPlayer, int N) {
 
     int grid[N+2][N+2];
-    int i =N+2, j;
+    int i=N+2, j;
 
     printf("Joueur %d a vous de jouer\n", currentPlayer);
 
     j = columnChoice(N);
 
+
     if (grid[i][j] == 1 || grid[i][j] == 2) {
         while (grid[i][j] != 0) {
             i = i - 1;
         }
-        grid[i][j]= currentPlayer;
-    } else {
-        grid[i][j]= currentPlayer;
     }
-
+    grid[i][j]= currentPlayer;
 }
