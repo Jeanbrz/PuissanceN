a5336f74634849d949dbe2a073ad7cf457f21765
PaulCantier <cantierpaulo@gmail.com>
2021-05-17 18:37:13 +0200

    modif partie logique

--- LOGIQUE/Logique.h
+++ LOGIQUE/Logique.h
@@ -1,3 +1,4 @@
 #include <stdbool.h>
 
-bool play(int currentPlayer, int N, int gridStatus);
+bool play(int currentPlayer, int N, int gridStatus[N+2][N+2]);
+int addValue(int j, int N, int gridStatus[N+2][N+2], int currentPlayer);
