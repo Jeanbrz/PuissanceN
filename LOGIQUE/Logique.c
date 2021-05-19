c628d7bf63c3ef33c9fe055e0a4c9370ea6bc262
PaulCantier <cantierpaulo@gmail.com>
2021-05-19 19:38:00 +0200

    ajout fct delete

--- LOGIQUE/Logique.c
+++ LOGIQUE/Logique.c
@@ -17,6 +17,10 @@ bool play(int currentPlayer, int N_COLS, int *grid) {
         j = columnChoice(N_COLS);
         addValue(j, N_COLS, grid, currentPlayer);
     }
+    if (choice==2){
+        j = columnChoice(N_COLS);
+        deleteValue(j, N_COLS, grid, currentPlayer);
+    }
     return false;
 }
 
@@ -27,12 +31,14 @@ int addValue(int j, int N_COLS, int *gridToUpdate, int currentPlayer) {
 
     currentCellAdress = gridToUpdate + i * N_COLS + j;
     currentCell = *(currentCellAdress);
+
     while (currentCell != 0) {
+
         if ((i == 0) && (currentCell != 0)) {
 
             printf("veuillez resaisir une colonne\n");
             j = columnChoice(N_COLS);
-            i =  N_COLS - 1;
+            i =  N_COLS;
         }
         i = i - 1;
 
@@ -45,3 +51,46 @@ int addValue(int j, int N_COLS, int *gridToUpdate, int currentPlayer) {
 
     return 0;
 }
+
+int deleteValue(int j, int N_COLS, int *gridToUpDown, int currentPlayer) {
+
+    int i = 0, currentCell, *currentCellAdress;
+
+    currentCellAdress = gridToUpDown + i * N_COLS + j;
+    currentCell = *(currentCellAdress);
+
+    while (currentCell!=getNextPlayer(currentPlayer)) {
+
+        while (currentCell == 0) {
+
+            if (i == N_COLS-1 ) {
+
+                printf("veuillez resaisir une colonne\n");
+                j = columnChoice(N_COLS);
+                i = N_COLS - 2;
+            }
+
+            i = i + 1;
+            currentCellAdress = gridToUpDown + i * N_COLS + j;
+            currentCell = *(currentCellAdress);
+        }
+
+        currentCellAdress = gridToUpDown + i * N_COLS + j;
+        currentCell = *(currentCellAdress);
+
+            if (currentCell == currentPlayer) {
+
+                printf("vous ne pouvez pas enlever votre propre jeton, veuillez resaisir une colonne valable\n");
+                j = columnChoice(N_COLS);
+
+            }
+
+    }
+
+    *(currentCellAdress) = 0;
+    displayGrid(N_COLS, gridToUpDown, 2);
+
+return 0;
+
+}
+
--- LOGIQUE/Logique.h
+++ LOGIQUE/Logique.h
@@ -3,3 +3,5 @@
 bool play(int currentPlayer, int N_COLS, int *grid);
 
 int addValue(int j, int N_COLS, int *gridToUpdate, int currentPlayer);
+
+int deleteValue(int j, int N_COLS, int *gridToUpDown, int currentPlayer);
